d8c2f5a312813525955554b4dccc58f66426c8cf
unknown <sasuke031026@gmail.com>
2023-02-19 22:13:56 +0900

    update

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ client.on("ready", function () {
     console.log("Ready for some good old ping pong!");
 });
 
+client.on("message", msg => {
+    console.log(msg.content);
+})
 
 client.login(process.env.CLIENT_TOKEN);
